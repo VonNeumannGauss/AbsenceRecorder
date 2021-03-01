ee7b284a44025d84c300d2971c6d8aa176237d71
Zach Marinov <Marinov.Z@etoncollege.org.uk>
2021-03-01 18:09:35 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Marinov, Zachary (Coll) on 01/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
