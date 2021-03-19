66bc17bebd3b595f3aa0e7aef83184beddad0cf4
Zach Marinov <Marinov.Z@etoncollege.org.uk>
2021-03-19 10:25:38 +0000

    Built extension to FileManager for loading data from a file

--- AbsenceRecorder/Extensions/FileManager.swift
+++ AbsenceRecorder/Extensions/FileManager.swift
@@ -7,4 +7,17 @@
 
 import Foundation
 
-
+extension FileManager {
+    func loadFromFile<T: Decodable>(to url: URL, decodableType: T) -> T?{
+        if let data = try? Data(contentsOf: url) {
+            let decoder = JSONDecoder()
+            if let loaded = try? decoder.decode(T.self, from: data) {
+                return loaded
+            }
+        }
+        return nil
+    }
+    
+    
+    
+}
